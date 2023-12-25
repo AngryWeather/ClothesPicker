152a3e2e83e8d1a8d0d4cf13c54a714cdff94599
Weronika <WerRic96@protonmail.com>
2023-12-25 15:28:34 +0100

    Serve server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"clothesPicker/server"
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(server.ClothingServer)
+	log.Fatal(http.ListenAndServe("localhost:5000", handler))
+}
