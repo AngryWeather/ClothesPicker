c18473e8691a531d521a250d882af0775b7e0b23
Weronika <WerRic96@protonmail.com>
2023-12-26 03:07:41 +0100

    Return random element for inMemoryStruct

--- cmd/main.go
+++ cmd/main.go
@@ -3,17 +3,26 @@ package main
 import (
 	"clothesPicker/server"
 	"log"
+	"math/rand"
 	"net/http"
 )
 
-type InMemoryClothesStore struct{}
+type InMemoryClothesStore struct {
+	clothes []server.Clothes
+}
 
 func (i *InMemoryClothesStore) GetRandomClothing() server.Clothes {
-	return server.Clothes{}
+	return i.clothes[rand.Intn(len(i.clothes))]
 }
 
 func main() {
-	server := &server.ClothesServer{Store: &InMemoryClothesStore{}}
+	clothes := []server.Clothes{
+		{Name: "blue jeans"},
+		{Name: "blue sweater"},
+		{Name: "red hoodie"},
+	}
+	store := InMemoryClothesStore{clothes}
+	server := &server.ClothesServer{Store: &store}
 
 	log.Fatal(http.ListenAndServe("localhost:5000", server))
 }
