d3484e10e7f774125d31f0fb6911a76f548908d3
Weronika <WerRic96@protonmail.com>
2023-12-30 19:21:53 +0100

    Add integration test

---
+++ cmd/server_integration._test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"bytes"
+	"clothesPicker/server"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func TestRecordingClothesAndRetrieving(t *testing.T) {
+	store := InMemoryClothesStore{}
+	sv := server.NewClothesServer(&store)
+
+	sv.ServeHTTP(httptest.NewRecorder(), newClothesRequest("blue jeans"))
+	sv.ServeHTTP(httptest.NewRecorder(), newClothesRequest("red hoodie"))
+	sv.ServeHTTP(httptest.NewRecorder(), newClothesRequest("black jeans"))
+
+	response := httptest.NewRecorder()
+	sv.ServeHTTP(response, newGetAllClothesRequest())
+
+	if response.Code != http.StatusOK {
+		t.Errorf("got status %d, want %d", response.Code, http.StatusOK)
+	}
+
+	var clothes server.Clothes
+
+	err := json.NewDecoder(response.Body).Decode(&clothes)
+
+	if err != nil {
+		t.Fatalf("Unable to parse response from %v", clothes)
+	}
+
+	want := server.Clothes{"blue jeans", "red hoodie", "black jeans"}
+	if !reflect.DeepEqual(clothes, want) {
+		t.Errorf("got %v, want %v", response.Body, want)
+	}
+}
+
+func newGetAllClothesRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/clothes", nil)
+	return req
+}
+
+func newClothesRequest(name string) *http.Request {
+	requestBody, _ := json.Marshal(name)
+	request, _ := http.NewRequest(http.MethodPost, "/clothes", bytes.NewBuffer(requestBody))
+
+	return request
+}
