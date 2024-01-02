1d4a48e858ef95e4388e2d9bc421e3985a2f7f2f
Weronika <WerRic96@protonmail.com>
2024-01-02 23:40:43 +0100

    Return 404 on id 0

--- server/server.go
+++ server/server.go
@@ -59,7 +59,7 @@ func (c *ClothesServer) clothesHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (c *ClothesServer) isIdValid(id int) bool {
-	return id > len(c.Store.GetAllClothes())
+	return id > len(c.Store.GetAllClothes()) || id == 0
 }
 
 func (c *ClothesServer) getClothesById(w http.ResponseWriter, id int) {
